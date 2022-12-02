1ec9d95b642640414205a29d32e5e0f4068154ce
Thomas Cooper <thomascooperca@gmail.com>
2022-12-02 18:35:13 -0500

    day 2

--- 2/main.go
+++ 2/main.go
@@ -19,27 +19,37 @@ func main() {
 		log.Fatal(err)
 	}
 
-	choices := map[string]int{"X": 1, "Y": 2, "Z": 3, "A": 1, "B": 2, "C": 3}
-	winners := map[string]string{"X": "C", "Y": "A", "Z": "B"}
-	// A,X rock -> beats C,Z scissors
-	// B,Y paper -> beats A,X rock
-	// C,Z scissors -> beats B,Y paper
+	//choices := map[string]int{"X": 1, "Y": 2, "Z": 3, "A": 1, "B": 2, "C": 3}
+	//winners := map[string]string{"X": "C", "Y": "A", "Z": "B"}
+	expected := map[string]map[string]int{"X": {"A": 3, "B": 1, "C": 2}, "Y": {"A": 1, "B": 2, "C": 3}, "Z": {"A": 2, "B": 3, "C": 1}}
+	// A,X(1) rock -> beats C,Z(3) scissors
+	// B,Y(2) paper -> beats A,X(1) rock
+	// C,Z(3) scissors -> beats B,Y(2) paper
 	// lose = 0, draw = 3, win  = 6
+	// X = lose, Y = draw, Z = win
 	var sum int
 	lines := strings.Split(string(in[:]), "\n")
 	fmt.Printf("Line count: %d\n", len(lines))
 	for _, line := range lines {
 		parts := strings.Split(line, " ")
 		theirs := parts[0]
-		mine := parts[1]
-		sum += choices[mine]
-		if choices[mine] == choices[theirs] {
+		outcome := parts[1]
+		if outcome == "Y" {
 			sum += 3
-			continue
 		}
-		if strings.Compare(theirs, winners[mine]) == 0 {
+		if outcome == "Z" {
 			sum += 6
 		}
+		sum += expected[outcome][theirs]
+		//mine := parts[1]
+		//sum += choices[mine]
+		//if choices[mine] == choices[theirs] {
+		//	sum += 3
+		//	continue
+		//}
+		//if strings.Compare(theirs, winners[mine]) == 0 {
+		//	sum += 6
+		//}
 	}
 	log.Printf("%d", sum)
 }
